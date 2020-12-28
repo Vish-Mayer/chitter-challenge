a70f4f04d84fb244b8d49e89a15a92ac06614ccd
Vish Mayer <vish.mayer@gmail.com>
2020-12-28 11:32:52 +0000

    refactored current date into env variable

--- lib/data_convertor.rb
+++ lib/data_convertor.rb
@@ -2,11 +2,14 @@ require 'date'
 
 class Convert
 
-  def self.date(date)
+  CURRENT_DATE = (DateTime.new(Time.now.strftime('%Y').to_i, Time.now.strftime('%m').to_i , Time.now.strftime('%d').to_i ))
+
+  def self.date(date, current_date = CURRENT_DATE)
     value = date.split("-")
     input_date = (DateTime.new(value[0].to_i, value[1].to_i, value[2].to_i))
-    current_date = (DateTime.new(Time.now.strftime('%Y').to_i, Time.now.strftime('%m').to_i , Time.now.strftime('%d').to_i ))
+
     answer = current_date - input_date
+
     if answer == (0/1)
       "today"
     end
--- spec/date_convertor_spec.rb
+++ spec/date_convertor_spec.rb
@@ -3,12 +3,12 @@ require 'data_convertor'
 describe Convert do
 
   describe '.date' do
-    it 'converts todays date to the word today' do
+    it 'returns "today" if date is eq to current date' do
       expect(Convert.date(Time.now.strftime("%Y-%m-%d"))).to eq 'today'
     end
 
-    # it 'converts todays date to the word today' do
-    #   expect(Convert.date('2015-10-2')).to eq 'today'
+    # it 'returns "yesterday" if (current date - date) is eq to 1' do
+    #   expect(Convert.date('2020-10-10')).to eq 'today'
     # end
   end
 end
