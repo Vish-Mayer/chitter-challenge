12e1c664fbd7b8f22c0eaedcc9ba8ec80602d665
Vish Mayer <vish.mayer@gmail.com>
2021-01-03 19:19:00 +0000

    edge case when no user found in mention

--- app/models/mention_user.rb
+++ app/models/mention_user.rb
@@ -3,14 +3,17 @@ class MentionUser
   def self.create(peep_id:, user_id:, mentioned_user:)
 
     mentioned_user_id = DatabaseConnection.query("SELECT id FROM users WHERE username = '#{mentioned_user}'")
-
-    result = DatabaseConnection.query(
-      "INSERT
-      INTO mention_user (peep_id, user_id, mentioned_user_id)
-      VALUES('#{peep_id}', '#{user_id}', '#{mentioned_user_id[0]['id']}')
-      RETURNING peep_id, user_id, mentioned_user_id;"
-    )
-    MentionUser.new(peep_id: result[0]['peep_id'], user_id: result[0]['user_id'], mentioned_user_id: result[0]['mentioned_user_id'])
+    if mentioned_user_id.any?
+      result = DatabaseConnection.query("
+        INSERT
+        INTO mention_user (peep_id, user_id, mentioned_user_id)
+        VALUES('#{peep_id}', '#{user_id}', '#{mentioned_user_id[0]['id']}')
+        RETURNING peep_id, user_id, mentioned_user_id;
+      ")
+      MentionUser.new(peep_id: result[0]['peep_id'], user_id: result[0]['user_id'], mentioned_user_id: result[0]['mentioned_user_id'])
+    else
+      nil
+    end
   end
 
   attr_reader :peep_id, :user_id, :mentioned_user_id
--- spec/mention_user_spec.rb
+++ spec/mention_user_spec.rb
@@ -15,5 +15,15 @@ describe MentionUser do
       expect(mention.user_id).to eq user.id
       expect(mention.mentioned_user_id).to eq user2.id
     end
+
+    it "does not create a mention if the gathered @mention does not exist" do
+
+      user = User.create(username: 'test_username', email: 'test@testmail.com', password: 'password123')
+      user2 = User.create(username: 'mentioned_user', email: 'mentioned@testmail.com', password: 'password123')
+      peep = Peep.create(body: 'this is a test mention @non-existing-user')
+      mention = MentionUser.create(peep_id: peep.id, user_id: user.id, mentioned_user: '@non-existing-user')
+
+      expect(mention).to eq nil
+    end
   end
 end
